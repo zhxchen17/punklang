77a7819ea29d3fd8823cd3549ed2d40cb6a111a6
zhxchen17 <zhxchen17@outlook.com>
2019-05-30 16:56:26 +0800

    format code to Unix EOLs

--- punkc_fs/format.sh
+++ punkc_fs/format.sh
@@ -2,5 +2,5 @@ mv frontend/parse.fs frontend/parse.fx
 mv frontend/parse.fsi frontend/parse.fxi
 mv frontend/lex.fs frontend/lex.fx
 fantomas . --recurse
-for f in $(find . -name "*.fx"); mv $f ${f%.*}.fs done
-for f in $(find . -name "*.fxi"); mv $f ${f%.*}.fsi done
+for f in $(find . -name "*.fx"); do mv $f ${f%.*}.fs; done
+for f in $(find . -name "*.fxi"); do mv $f ${f%.*}.fsi; done
